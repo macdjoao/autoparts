2381223dcb8df5070beb2301e50819de96b55874
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2022-12-12 21:15:13 -0300

    Criando primeiros schemas com pydantic

---
+++ autoparts/schemas.py
@@ -0,0 +1,56 @@
+from pydantic import BaseModel
+
+# User
+
+class User(BaseModel):
+    user_name: str
+    user_email: str
+
+class CreateUser(User):
+    user_password: str
+
+class ReadUser(User):
+    user_id: int
+
+class DeleteUser(BaseModels):
+    user_email: str
+
+# Brand
+
+class Brand(BaseModel):
+    brand_name: str
+    brand_code: str
+
+class ReadBrand(Brand):
+    brand_id: str
+
+class DeleteBrand(BaseModels):
+    brand_code: str
+
+# Model
+
+class Model(BaseModel):
+    model_name: str
+    model_code: str   
+    brand_code: str
+
+class ReadModel(Model):
+    model_id: str
+
+class DeleteBrand(BaseModels):
+    model_code: str
+
+# Part
+
+class Part(BaseModel):
+    part_name: str
+    part_code: str   
+    part_price: str
+    part_amount: str
+    model_code: str
+
+class ReadPart(Part):
+    part_id: str
+
+class DeleteBrand(BaseModels):
+    part_code: str
