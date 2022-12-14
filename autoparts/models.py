b304688db6e486d23c1ebeec561ebc5aea5c02d7
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2022-12-14 20:20:47 -0300

    Alteração nos nomes dos campos dos modelos

--- autoparts/models.py
+++ autoparts/models.py
@@ -1,7 +1,7 @@
 # coding: utf-8
-from sqlalchemy import DECIMAL, Column, ForeignKey, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, DECIMAL, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 metadata = Base.metadata
@@ -10,27 +10,27 @@ metadata = Base.metadata
 class Brand(Base):
     __tablename__ = 'brands'
 
-    brand_id = Column(Integer, primary_key=True, unique=True)
-    brand_name = Column(String(100), nullable=False, unique=True)
-    brand_code = Column(String(3), nullable=False, unique=True)
+    id = Column(Integer, primary_key=True, unique=True)
+    name = Column(String(255), nullable=False, unique=True)
+    code = Column(String(3), nullable=False, unique=True)
 
 
 class User(Base):
     __tablename__ = 'users'
 
-    user_id = Column(Integer, primary_key=True, unique=True)
-    user_name = Column(String(100), nullable=False)
-    user_email = Column(String(100), nullable=False)
-    user_password = Column(String(255), nullable=False)
+    id = Column(Integer, primary_key=True, unique=True)
+    name = Column(String(255), nullable=False)
+    email = Column(String(255), nullable=False, unique=True)
+    password = Column(String(255), nullable=False)
 
 
 class Model(Base):
     __tablename__ = 'models'
 
-    model_id = Column(Integer, primary_key=True, unique=True)
-    model_name = Column(String(100), nullable=False, unique=True)
-    model_code = Column(String(3), nullable=False, unique=True)
-    brand_code = Column(ForeignKey('brands.brand_code'), index=True)
+    id = Column(Integer, primary_key=True, unique=True)
+    name = Column(String(255), nullable=False, unique=True)
+    code = Column(String(3), nullable=False, unique=True)
+    brand_code = Column(ForeignKey('brands.code'), index=True)
 
     brand = relationship('Brand')
 
@@ -38,11 +38,11 @@ class Model(Base):
 class Part(Base):
     __tablename__ = 'parts'
 
-    part_id = Column(Integer, primary_key=True, unique=True)
-    part_name = Column(String(100), nullable=False, unique=True)
-    part_code = Column(String(3), nullable=False, unique=True)
-    part_price = Column(DECIMAL(10, 0), nullable=False)
-    part_amount = Column(Integer, nullable=False)
-    model_code = Column(ForeignKey('models.model_code'), index=True)
+    id = Column(Integer, primary_key=True, unique=True)
+    name = Column(String(255), nullable=False, unique=True)
+    code = Column(String(3), nullable=False, unique=True)
+    price = Column(DECIMAL(10, 0), nullable=False)
+    amount = Column(Integer, nullable=False)
+    model_code = Column(ForeignKey('models.code'), index=True)
 
     model = relationship('Model')
